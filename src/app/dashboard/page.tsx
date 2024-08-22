086fdc004cc8aeadb59293247ef8e2c0e7714ab9
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2024-08-21 19:23:52 -0600

    chore: Update npm dependencies and add React Query provider

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,9 @@
 'use client'
 
-import ButtonAuth from "@/components/ui/ButtonAuth";
-import { useSession } from "next-auth/react";
+import MaintenancePage from "@/components/ui/maintenance-page"
 
 export default function DashboardPage() {
-    const { data: session, status } = useSession();
-
-    if (status === "loading") {
-        return <p>Loading...</p>;
-    }
-    console.log(session);
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <pre>
-                <code>{JSON.stringify(session, null, 2)}</code>
-            </pre>
-            <ButtonAuth />
-        </div>
+       <MaintenancePage title="dashboard" />
     )
 }
