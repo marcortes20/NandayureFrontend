7235f1f5e861b0ec95910ebe0ef41cb66955aa40
Adriana-06 <adrianamorera126@gmail.com>
2024-09-04 09:34:24 -0600

    Ignorar

--- src/middleware.ts
+++ src/middleware.ts
@@ -53,9 +53,9 @@ export async function middleware(req: NextRequest) {
 
   // Allow access to public auth routes
   if (pathname.startsWith('/auth')) {
-    if (pathname === '/auth/register') {
-      return await rolesMiddleware(req, [Roles.rh]);
-    }
+   // if (pathname === '/auth/register') {
+     // return await rolesMiddleware(req, [Roles.rh]);
+    //}
     return NextResponse.next();
   }
 
