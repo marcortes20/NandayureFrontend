4a164e74a5df9f42b90c339499f1bcb7f8458d78
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2024-09-05 22:41:50 -0600

    refactor: Update employee profile page
    - Refactor the employee profile page to use the updated employeeId field instead of the deprecated id field.
    - Replace the useSession hook with useGetEmployeeId hook to retrieve the employeeId.
    - Update the profile fields to use the correct field names.
    - Remove unused code and imports.

--- src/components/profile/dialog/dialog.tsx
+++ src/components/profile/dialog/dialog.tsx
@@ -10,6 +10,9 @@ import {
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import useGetEmployeeId from '@/hooks/common/useGetEmployeeId';
+import useUpdateEmployee from '@/hooks/profile/useUpdateEmployee';
+import { useState } from 'react';
 
 interface Field {
   id: string;
@@ -29,36 +32,51 @@ export function DialogProfile({
   title,
   description,
 }: DialogProfileProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const { employeeId } = useGetEmployeeId();
+  const { onSubmit, register } = useUpdateEmployee({
+    employeeId: employeeId,
+    setIsOpen,
+  });
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
+        <Button variant="outline">Editar</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[450px]">
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
           <DialogDescription>{description}</DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          {fields.map((field) => (
-            <div key={field.id} className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor={field.id} className="text-right">
-                {field.label}
-              </Label>
-              <Input
-                id={field.id}
-                defaultValue={field.defaultValue}
-                type={field.type || 'text'}
-                className="col-span-3"
-              />
-            </div>
-          ))}
-        </div>
-        <DialogFooter>
-          <Button type="submit" className=''>
-            Guardar
-          </Button>
-        </DialogFooter>
+        <form onSubmit={onSubmit}>
+          <div className="grid gap-4 py-4">
+            {fields.map((field) => (
+              <div
+                key={field.id}
+                className="grid grid-cols-4 items-center gap-4"
+              >
+                <Label htmlFor={field.id} className="text-right">
+                  {field.label}
+                </Label>
+                <Input
+                  id={field.id}
+                  defaultValue={field.defaultValue}
+                  type={field.type || 'text'}
+                  className="col-span-3"
+                  {...register(field.id)}
+                />
+              </div>
+            ))}
+          </div>
+          <DialogFooter>
+            <Button
+              type="submit"
+              className=" mt-4 py-2 px-4 bg-dodger-blue-600 hover:bg-dodger-blue-700 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+            >
+              Guardar
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
--- src/hooks/profile/useUpdateEmployee.ts
+++ src/hooks/profile/useUpdateEmployee.ts
@@ -1,8 +1,43 @@
+import { updateEmployee } from '@/server/profile/updateEmployee/actions';
+import { Employee, UpdateEmployee } from '@/types/entities';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+
 interface Props {
   employeeId: number | undefined;
+  setIsOpen: (value: boolean) => void;
 }
 
-const useUpdateEmployee = ({ employeeId }: Props) => {
-  
+const useUpdateEmployee = ({ employeeId, setIsOpen }: Props) => {
+  const { register, handleSubmit } = useForm();
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: async (data: UpdateEmployee) =>
+      await updateEmployee({ employeeId: employeeId!, employee: data }),
+    mutationKey: ['updateEmployee', employeeId],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['employeeById', employeeId] });
+    },
+  });
+
+  const onSubmit = handleSubmit(async (data: UpdateEmployee) => {
+    try {
+      await toast.promise(mutation.mutateAsync(data), {
+        loading: 'Actualizando empleado...',
+        success: 'Empleado actualizado',
+        error: 'Error al actualizar empleado',
+      });
+      setIsOpen(false);
+    } catch (error: any) {
+      console.error(error);
+      setIsOpen(false);
+    }
+  });
+  return {
+    mutation,
+    onSubmit,
+    register,
+  };
 };
 export default useUpdateEmployee;
