d6013550a471510ef310ca3ac3c8c40618134e2c
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2024-09-10 09:23:07 -0600

    Refactor: Add form validation and error handling in DialogProfile component

--- src/hooks/profile/useUpdateEmployee.ts
+++ src/hooks/profile/useUpdateEmployee.ts
@@ -1,5 +1,7 @@
+import { UpdateEmployeeSchema } from '@/lib/zod';
 import { updateEmployee } from '@/server/profile/updateEmployee/actions';
 import { UpdateEmployee } from '@/types/entities';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
@@ -10,8 +12,16 @@ interface Props {
 }
 
 const useUpdateEmployee = ({ employeeId, setIsOpen }: Props) => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(UpdateEmployeeSchema)
+  });
+
   const queryClient = useQueryClient();
+
   const mutation = useMutation({
     mutationFn: async (data: UpdateEmployee) =>
       await updateEmployee({ employeeId: employeeId!, employee: data }),
@@ -52,6 +62,7 @@ const useUpdateEmployee = ({ employeeId, setIsOpen }: Props) => {
     mutation,
     onSubmit,
     register,
+    errors,
   };
 };
 
