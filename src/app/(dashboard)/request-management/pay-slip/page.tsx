b88328d956c5cdd16373ce8954297d6e20e4be6c
José Pablo <suarezgomezjosepablo03@gmail.com>
2024-09-27 10:57:57 -0600

    add-new pages

---
+++ src/app/(dashboard)/request-management/pay-slip/page.tsx
@@ -0,0 +1,7 @@
+import MaintenancePage from '@/components/ui/maintenance-page';
+
+const RequestManagementPaySlipPage = () => {
+  return <MaintenancePage title="Boleta de pago" />;
+};
+
+export default RequestManagementPaySlipPage;
---
+++ src/app/(dashboard)/request-management/vacation-request/page.tsx
@@ -0,0 +1,7 @@
+import MaintenancePage from '@/components/ui/maintenance-page';
+
+const RequestManagementVacactionRequest = () => {
+  return <MaintenancePage title="Solicitud de vacaciones" />;
+};
+
+export default RequestManagementVacactionRequest;
