56499596cca75e3c09a36711ccc43302e1ac98ca
José Pablo <suarezgomezjosepablo03@gmail.com>
2024-10-01 22:30:13 -0600

    .

--- src/components/templates/salary-certificate/Salary-certificate-template.tsx
+++ src/components/templates/salary-certificate/Salary-certificate-template.tsx
@@ -1,15 +1,21 @@
 'use client';
 import {
-  PDFViewer,
   View,
   Document,
   Page,
   Text,
   Image,
+  StyleSheet,
 } from '@react-pdf/renderer';
 import useGetSalaryCertificateTemplate from '@/hooks/templates/salary-certificate/useSalaryCertificateTemplates';
-import { styles } from './style';
-import logo from '@/assets/logoNanda.jpg';
+import dynamic from 'next/dynamic';
+const PDFViewer = dynamic(
+  () => import('@react-pdf/renderer').then((mod) => mod.PDFViewer),
+  {
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+  },
+);
 interface HorizontalLineProps {
   width: string;
   spaceWidth: string;
@@ -109,6 +115,7 @@ const HorizontalLineFooter: React.FC<HorizontalLineProps> = ({
 const ConstanciaSalario: React.FC = () => {
   const { SalaryCertificateInfo, isLoading } =
     useGetSalaryCertificateTemplate();
+  console.log(SalaryCertificateInfo);
   return (
     <div>
       {!isLoading ? (
@@ -168,43 +175,45 @@ const ConstanciaSalario: React.FC = () => {
                       style={[styles.breakdownRow, styles.yellowBackground]}
                     >
                       <Text>CCSS 9,67%:</Text>
-                      <Text>¢{SalaryCertificateInfo?.deductions.ccss}</Text>
-  
+                      <Text>¢{SalaryCertificateInfo?.deductions?.ccss}</Text>
                     </View>
                     <View
                       style={[styles.breakdownRow, styles.yellowBackground]}
                     >
                       <Text>BANCO POPULAR 1%:</Text>
                       <Text>
-                        ¢{SalaryCertificateInfo?.deductions.bancoPopular}
+                        ¢{SalaryCertificateInfo?.deductions?.bancoPopular}
                       </Text>
                     </View>
                     <View style={styles.breakdownRow}>
                       <Text>TRIBUTACIÓN Código Trabajo:</Text>
-                      <Text>¢{SalaryCertificateInfo?.deductions.tax}</Text>
+                      <Text>¢{SalaryCertificateInfo?.deductions?.tax}</Text>
                     </View>
                     {/* Sección con fondo rojo */}
                     <View style={[styles.breakdownRow, styles.redBackground]}>
                       <Text>COOPEANDE Opcional:</Text>
                       <Text>
-                        ¢{SalaryCertificateInfo?.deductions.coopeAnde}
+                        ¢{SalaryCertificateInfo?.deductions?.coopeAnde}
                       </Text>
                     </View>
                     <View style={[styles.breakdownRow, styles.redBackground]}>
                       <Text>ASEMUNA 3%:</Text>
-                      <Text>¢{SalaryCertificateInfo?.deductions.asemuna}</Text>
+                      <Text>¢{SalaryCertificateInfo?.deductions?.asemuna}</Text>
                     </View>
                     <View style={[styles.breakdownRow, styles.redBackground]}>
                       <Text>FUNERALES:</Text>
                       <Text>
                         ¢
-                        {SalaryCertificateInfo?.deductions.lifeFuneralInsurance}
+                        {
+                          SalaryCertificateInfo?.deductions
+                            ?.lifeFuneralInsurance
+                        }
                       </Text>
                     </View>
                     <View style={[styles.breakdownRow, styles.redBackground]}>
                       <Text>SERVICOOP:</Text>
                       <Text>
-                        ¢{SalaryCertificateInfo?.deductions.servicoop}
+                        ¢{SalaryCertificateInfo?.deductions?.servicoop}
                       </Text>
                     </View>
 
@@ -274,3 +283,158 @@ const ConstanciaSalario: React.FC = () => {
 };
 
 export default ConstanciaSalario;
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 30,
+    fontFamily: 'Times-Roman',
+    width: '100%',
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: '90%',
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  tituloContainer: {
+    flex: 1,
+    textAlign: 'center',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 14,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+  },
+  subtitulo: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginBottom: 15,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+  },
+  body: {
+    fontSize: 12,
+    marginBottom: 15,
+    textAlign: 'justify',
+    color: '#333333',
+  },
+  boldText: {
+    fontWeight: 'bold',
+    marginVertical: 5,
+    position: 'relative',
+    top: -5,
+    color: '#00000',
+  },
+  containerCenter: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 10,
+  },
+  breakdownContainer: {
+    width: '60%',
+    fontSize: 12,
+    border: '1pt solid black',
+    padding: 2,
+  },
+  breakdownRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 3,
+    borderBottom: '1pt solid black',
+  },
+  lastRow: {
+    borderBottom: 'none',
+  },
+  emptySpace: {
+    height: 6,
+    borderBottom: 'none',
+  },
+  signature: {
+    marginTop: 5,
+    textAlign: 'center',
+    fontSize: 12,
+  },
+  atentamente: {
+    marginTop: 7,
+    textAlign: 'center',
+    fontSize: 12,
+    color: '#333333',
+  },
+  signatureName: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: '#333333',
+  },
+  personalID: {
+    fontSize: 12,
+    color: '#333333',
+  },
+
+  horizontalLine: {
+    borderBottomWidth: 2,
+    marginVertical: 0,
+  },
+  horizontalLine2: {
+    borderBottomWidth: 10,
+    marginVertical: 1,
+  },
+  space: {
+    width: '1%',
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    fontSize: 10,
+    borderTop: '0.5pt solid',
+    height: 80,
+    flexDirection: 'column',
+    backgroundColor: '#e0e0e0',
+    paddingHorizontal: 20,
+    paddingTop: 10,
+    justifyContent: 'space-between',
+  },
+  footerLeft: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    paddingLeft: 10,
+  },
+  footerRightContainer: {
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    marginTop: -50,
+  },
+  footerRight: {
+    textAlign: 'right',
+    marginTop: 5,
+  },
+
+  horizontalLineFooter: {
+    width: '100%',
+    borderBottomWidth: 5,
+    borderBottomColor: '#000',
+    marginVertical: 5,
+  },
+
+  redBackground: {
+    backgroundColor: '#f28b82',
+  },
+  yellowBackground: {
+    backgroundColor: '#fff475',
+  },
+});
