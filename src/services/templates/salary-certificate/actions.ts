9e11fa35d18a8bf84374986803dc1b29ca64f781
José Pablo <suarezgomezjosepablo03@gmail.com>
2024-10-04 09:21:48 -0600

    Refactor middleware to protect all private routes

--- src/services/templates/salary-certificate/actions.ts
+++ src/services/templates/salary-certificate/actions.ts
@@ -9,7 +9,7 @@ export async function getSalaryCertificateInfo({ id }: { id: string }) {
   };
 
   const res = await fetch(
-    // TODO - Change the URL to the correct one
+    // TODO:  Change the URL to the correct one
     `https://66331bf7f7d50bbd9b48366c.mockapi.io/Test/salary-certificate/${id}`,
     options,
   );
